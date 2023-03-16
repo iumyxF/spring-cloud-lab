7cabff2c9012eed9df4f2c7dd31804c79f7da044
iumyxF <1119759672@qq.com>
2023-03-16 11:23:06 +0800

    feat(cloud-gateway): ✨ gateway的简单使用

---
+++ cloud-gateway/src/main/java/com/exmaple/gateway/GateWayApplication.java
@@ -0,0 +1,16 @@
+package com.exmaple.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @Date 2023/3/16 10:01
+ * @Author iumyxF
+ */
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
